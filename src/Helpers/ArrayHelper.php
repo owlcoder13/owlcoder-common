466779f9d0ad4f266bfb91b362b130266748e600
owl <owlcoder1@gmail.com>
2019-12-13 14:25:16 +0300

    fixes for libs

--- src/Helpers/ArrayHelper.php
+++ src/Helpers/ArrayHelper.php
@@ -6,5 +6,25 @@ use Closure;
 
 class ArrayHelper
 {
+    public static function getColumn($array, $column)
+    {
+        $out = [];
 
+        foreach ($array as $item) {
+            $out[] = DataHelper::get($item, $column);
+        }
+
+        return $out;
+    }
+
+    public static function first($array, $callback, $default = null)
+    {
+        foreach ($array as $key => $value) {
+            if ($callback($value, $key)) {
+                return $value;
+            }
+        }
+
+        return $default;
+    }
 }
