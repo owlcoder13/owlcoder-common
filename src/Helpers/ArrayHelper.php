5999e839bdfd4a82aca52a45fe98177104aaf9e5
owlcoder <owlcoder1@gmail.com>
2021-02-16 12:14:29 +0300

    additional functions in ArrayHelper.php

--- src/Helpers/ArrayHelper.php
+++ src/Helpers/ArrayHelper.php
@@ -2,8 +2,6 @@
 
 namespace Owlcoder\Common\Helpers;
 
-use Closure;
-
 class ArrayHelper
 {
     public static function getColumn($array, $column)
@@ -26,5 +24,69 @@ class ArrayHelper
         }
 
         return $default;
+    }
+
+    /**
+     * Простой маппинг, с помощью callback
+     *
+     * @param $callable
+     * @param $arr
+     * @return array
+     */
+    public static function map($callable, $arr)
+    {
+        $out = [];
+        foreach ($arr as $key => $one) {
+            $out[] = $callable($one, $key);
+        }
+
+        return $out;
+    }
+
+    /**
+     * Преобразует массив таким образом, что одно из полей элемента в массиве
+     * становится ключём нового ассоциативного массива
+     *
+     * @param $arr
+     * @param $field
+     * @return array
+     */
+    public static function mapFieldAsKey($arr, $field)
+    {
+        $out = [];
+
+        foreach ($arr as $one) {
+            $val = DataHelper::get($one, $field);
+            $out[$val] = $one;
+        }
+
+        return $out;
+    }
+
+    /**
+     * Фильтрует массив в соответствии с callback функцией
+     * Может сохранять ключи, при фильтрации, если требуется
+     *
+     * @param $arr
+     * @param $callback
+     * @param false $keepKeys
+     * @return array
+     */
+    public static function filter($arr, $callback, $keepKeys = false)
+    {
+        $out = [];
+
+        foreach ($arr as $key => $one) {
+
+            if ($callback($one, $key)) {
+                if ($keepKeys) {
+                    $out[$key] = $one;
+                } else {
+                    $out[] = $one;
+                }
+            }
+        }
+
+        return $out;
     }
 }
